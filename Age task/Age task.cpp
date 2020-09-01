b8c0cb1fb7f787eb6e9afb6c94e7191046e02b83
KingTusk <70224576+KingTusk@users.noreply.github.com>
2020-09-01 14:05:16 +0200

    task complete

--- Age task/Age task.cpp
+++ Age task/Age task.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+int age = 0;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "what is your age? ";
+    std::cin >> age;
+    if (age > 19) {
+        std::cout << "You are an adult.";
+    }
+    else {
+        std::cout << "you are a teenager.";
+    }
 }
